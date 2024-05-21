557c8869b5e77af5d6ffb4027a6a025a2437453e
DuempelmannLea <lea.duempelmann@unibe.ch>
2024-05-21 14:58:37 +0200

    Update symphony.R
    improved function

--- Annotation Transfer with Symphony/symphony.R
+++ Annotation Transfer with Symphony/symphony.R
@@ -17,53 +17,35 @@ source('./utils.R') # color definitions, symphony function and plotting function
 #00 global
 symphony(
   CELLTYPEref = "Tan_global",
-  CELLTYPEquery = "ENDO_global",
-  TanIntegrationVars = c("sample","stage"),
-  THETA_REF = c(2,1),
-  ENDOIntegrationVars = c("batch", "sample")
+  CELLTYPEquery = "ENDO_global"
 )
 
 #01 lymphocyte
 symphony(
   CELLTYPEref = "Tan_lymphocyte",
-  CELLTYPEquery = "ENDO_lymphocyte",
-  TanIntegrationVars = c("sample","stage"),
-  THETA_REF = c(2,1),
-  ENDOIntegrationVars = c("batch", "sample")
+  CELLTYPEquery = "ENDO_lymphocyte"
 )
 
 #02 myeloid
 symphony(
   CELLTYPEref = "Tan_myeloid",
-  CELLTYPEquery = "ENDO_myeloid",
-  TanIntegrationVars = c("sample","stage"),
-  THETA_REF = c(2,1),
-  ENDOIntegrationVars = c("batch", "sample")
+  CELLTYPEquery = "ENDO_myeloid"
 )
 
 #03 epithelial
 symphony(
   CELLTYPEref = "Tan_epithelial",
-  CELLTYPEquery = "ENDO_epithelial",
-  TanIntegrationVars = c("sample"),
-  THETA_REF = c(1.5),
-  ENDOIntegrationVars = c("batch", "sample")
+  CELLTYPEquery = "ENDO_epithelial"
 )
 
 #04 endothelial
 symphony(
   CELLTYPEref = "Tan_endothelial",
-  CELLTYPEquery = "ENDO_endothelial",
-  TanIntegrationVars = c("sample","stage"),
-  THETA_REF = c(2,1),
-  ENDOIntegrationVars = c("sample","batch")
+  CELLTYPEquery = "ENDO_endothelial"
 )
 
 #05 stromal fibroblasts
 symphony(
   CELLTYPEref = "Tan_stromal",
-  CELLTYPEquery = "ENDO_stromal",
-  TanIntegrationVars = c("sample","stage"),
-  THETA_REF = c(2,1),
-  ENDOIntegrationVars = c("batch", "sample")
+  CELLTYPEquery = "ENDO_stromal"
 )
